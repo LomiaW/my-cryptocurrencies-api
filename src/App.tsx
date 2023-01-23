8cb1bca8d39a3de31756e06d9e3140df47915f3d
Lomia Wu <rwu30@myseneca.ca>
2023-01-23 12:43:51 -0500

    update API to fetch coin price list

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,33 @@
-import React from 'react';
 import './App.css';
 import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+export type Crypto = {
+   "id": string;
+   "symbol": string;
+   "name": string;
+   "current_price": number;
+   "market_cap_rank": number;
+   "high_24h": number;
+   "low_24h": number;
+}
 
 function App() {
-   const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false';
-
    
+   const [cryptos, setCryptos] = useState<Crypto[] | null>(null);
+
+   useEffect(() => {
+      const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false';
+      axios.get(url).then((response) => {
+         setCryptos(response.data);
+      });
+   }, []);
 
    return (
       <div className="App">
-         Crypto Coin Currency
+         {cryptos ? cryptos.map((crypto) => {
+            return <p>{crypto.name + ' $' + crypto.current_price}</p>
+         }) : null}
       </div>
    );
 }
